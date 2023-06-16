32b1cbea6cfd058b764876cdaecb3a4bfba739cd
imBrunoMarquez <bruno.silva.marquez@gmail.com>
2023-06-16 11:35:15 +0100

    remove import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { QueryClientProvider, QueryClient } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
 import './styles/global.css';
 
 const queryClient = new QueryClient();
